55787db014abe3abc76de4c063e0146b5775b2fa
wahyunugrahha <wahyunih297@gmail.com>
2025-06-21 10:17:40 +0700

    Add Swagge Setup

--- src/application/web.js
+++ src/application/web.js
@@ -1,11 +1,25 @@
-import express from "express";
-import { publicRouter } from "../route/public-api.js";
-import { errorMiddleware } from "../middleware/error-middleware.js";
-import { userRouter } from "../route/api.js";
+// src/application/web.js
+import express from 'express';
+import {publicRouter} from '../route/public-api.js';
+import {userRouter} from '../route/api.js';
+import {errorMiddleware} from '../middleware/error-middleware.js';
+import swaggerUi from 'swagger-ui-express'; // Tambahkan ini
+import YAML from 'yamljs'; // Tambahkan ini
+import path from 'path'; // Tambahkan ini
 
-export const web = express();
+const web = express();
 web.use(express.json());
+
+// Load Swagger YAML file
+const swaggerDocument = YAML.load(path.resolve('docs', 'swagger.yaml')); // Sesuaikan path jika perlu
+
+// Serve Swagger UI
+web.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Ini adalah rute untuk Swagger UI
 
 web.use(publicRouter);
 web.use(userRouter);
 web.use(errorMiddleware);
+
+export {
+    web
+};
